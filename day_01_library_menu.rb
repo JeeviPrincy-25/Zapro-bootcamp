2d62fef503ab4e2ccf926dd4835b6531a77b25c9
Jeevitha Princy <jeevijeba2006@gmail.com>
2026-06-07 13:40:55 +0530

    Day 1 exercise: Spanish menu option

--- day_01_library_menu.rb
+++ day_01_library_menu.rb
@@ -1,5 +1,18 @@
+def show_spanish_menu
+  puts "\n-- Sistema de Gestion de Biblioteca --"
+  puts "1. Agregar un libro"
+  puts "2. Listar libros"
+  puts "3. Buscar un libro"
+  puts "4. Actualizar un libro"
+  puts "5. Eliminar un libro"
+  puts "6. Salir"
+
+  puts "\nPress Enter to return to the main menu"
+  gets
+end
 loop do
     puts "--Library Management System--"
+    puts "0. Spanish Menu"
     puts "1. Add a book"
     puts "2. List books"
     puts "3. Search for a book"
@@ -9,6 +22,8 @@ loop do
     print "Enter your choice : "
     ch=gets.chomp
     case ch
+    when "0"
+        show_spanish_menu
     when "1"
         puts "Add books - coming soon!"
     when "2"
